38106c5ce60e2b096432a8fbf435d8a69033c9c8
JoanneZamorano <joannezamorano@gmail.com>
2022-09-09 17:42:31 +0200

    FEAT: CLOUDINARY

--- src/api/posts/posts.controller.js
+++ src/api/posts/posts.controller.js
@@ -1,17 +1,18 @@
 
-// const { deleteFile } = require('../../utils/middlewares/deleteFile.middleware');
+const { deleteFile } = require('../../utils/middlewares/deleteFile.middleware');
 const Post = require('./posts.model');
 
 
 const postPost = async (req, res, next) =>{
     try {
-        const posts = await new Post(req.body);
-
+        const posts = new Post(req.body);
+        console.log(posts.image)
         //Vamos a recoger la imagen del formulario
         if(req.file){
-            posts.photo = req.file.path
-        }
+            posts.image = req.file.path
+        console.log(req.file.path)
 
+        }
         const savedPost = await posts.save();
         return res.status(200).json(savedPost);
     } catch (error) {
@@ -27,12 +28,12 @@ const putPost = async (req, res, next) =>{
         posts._id = id;
 
         if(req.file){
-            posts.photo = req.file.path;
+            posts.image = req.file.path;
         }
 
         const postsDb = await Post.findByIdAndUpdate(id, posts);
-        if(postsDb.photo){
-            deleteFile(postsDb.photo)
+        if(postsDb.image){
+            deleteFile(postsDb.image)
         }
         return res.status(200).json(postsDb);
     } catch (error) {
@@ -44,8 +45,8 @@ const deletePost = async (req, res, next) =>{
     try {
         const {id} = req.params;
         const posts = await Post.findByIdAndDelete(id);
-        if(posts.photo){
-            deleteFile(posts.photo);
+        if(posts.image){
+            deleteFile(posts.image);
         }
         return res.status(200).json(posts);
     } catch (error) {
